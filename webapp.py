03f5e2eacec6d1fe64e86b6eb40c118fbaf1fc5a
omar <omar_omarsalama@yahoo.com>
2020-11-16 19:35:13 +0100

    messaging problem

--- webapp.py
+++ webapp.py
@@ -7,16 +7,15 @@ from pydantic import Field, BaseModel
 TOKEN = os.environ.get('SLACK_TOKEN')
 
 Slacker = Slack(
-            TOKEN,
-            BearerAuth(TOKEN)
-        )
-
+    TOKEN,
+    BearerAuth(TOKEN)
+)
 
 
 channels, _ = Slacker.get_conversations()
 
 
-CHANNEL = channels['test']  #----> for testing
+CHANNEL = channels['test']  # ----> for testing
 # channel = channels['04-labeling-questions'] # main application channel
 
 # CHANNEL = channels['test']  #----> for testing
@@ -24,7 +23,7 @@ CHANNEL = channels['test']  #----> for testing
 
 class MessageModel(BaseModel):
     text: str
-    
+
 
 images = {}
 messages = {}
@@ -34,14 +33,14 @@ app = FastAPI()
 
 @app.post('/upload_message/{userID}')
 async def upload_message(userID: str, message: MessageModel):
-    messages[userID] = message.dict()['text']
+    messages[userID] = await message.dict()['text']
     print(messages)
     return {'type': 'success'}
 
 
 @app.post('/send_file/{userID}')
 async def send_file(userID: str, file: UploadFile = File(...)):
-    images[userID] = await file.read() 
+    images[userID] = await file.read()
     Slacker.send_file(CHANNEL, images[userID], messages[userID])
     return {"filename": file.filename}
 
